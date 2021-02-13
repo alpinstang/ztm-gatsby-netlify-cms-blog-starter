654ec971c7099a6f34cbfb5678e9e7a62fa6a8e7
r4pt0s <r4pt0s@protonmail.com>
2021-02-13 09:05:39 +0100

    setup plugins, config, cms

---
+++ src/cms/cms.js
@@ -0,0 +1,8 @@
+import CMS from "netlify-cms-app";
+
+// Not reliably loaded by registerPreviewStyle, so import directly
+
+// Add Previews
+
+// Extend editor
+//CMS.registerEditorComponent(ctaEditorConfig);
